4f5d4e3d9d7f4bf3718f87e4d09cec305de31f65
Ivan Paden <i.paden@tudelft.nl>
2022-03-29 15:00:49 +0200

    Add info

--- src/SurfaceLayer.h
+++ src/SurfaceLayer.h
@@ -1,3 +1,24 @@
+/*
+  Copyright (c) 2021-2022,
+  Ivan Pađen <i.paden@tudelft.nl>
+  3D Geoinformation,
+  Delft University of Technology
+
+  This file is part of City4CFD.
+
+  City4CFD is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  City4CFD is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
 #ifndef CITY4CFD_SURFACELAYER_H
 #define CITY4CFD_SURFACELAYER_H
 
