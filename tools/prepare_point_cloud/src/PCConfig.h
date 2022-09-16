3f3708b6e92b1387d12d38708f0c7225f7d50447
Ivan Paden <i.paden@tudelft.nl>
2022-09-16 14:28:14 +0200

    Point cloud preparation tool WIP

---
+++ tools/prepare_point_cloud/src/PCConfig.h
@@ -0,0 +1,162 @@
+/*
+  City4CFD
+ 
+  Copyright (c) 2021-2022, 3D Geoinformation Research Group, TU Delft  
+
+  This file is part of City4CFD.
+
+  City4CFD is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  City4CFD is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with City4CFD.  If not, see <http://www.gnu.org/licenses/>.
+
+  For any information or further details about the use of City4CFD, contact
+  Ivan Pađen
+  <i.paden@tudelft.nl>
+  3D Geoinformation Research Group
+  Delft University of Technology
+*/
+
+#ifndef CITY4CFD_PCCONFIG_H
+#define CITY4CFD_PCCONFIG_H
+
+#include "boost/filesystem.hpp"
+
+#include "valijson/adapters/nlohmann_json_adapter.hpp"
+#include "valijson/schema.hpp"
+#include "valijson/schema_parser.hpp"
+#include "valijson/validator.hpp"
+
+#include "PCconfigSchema.inc"
+
+namespace fs = boost::filesystem;
+
+namespace PCPrep {
+
+class Config {
+public:
+    static Config& get() {
+        static Config inst;
+        return inst;
+    }
+
+protected:
+    Config() = default;
+    Config(const Config&) = delete;
+    Config& operator=(const Config&) = delete;
+    ~Config() = default;
+
+public:
+    //-- Input 
+    std::vector<std::string> las_files;              // LAS point cloud
+    std::vector<int>         las_classes_ground;     // LAS classes used by terrain
+    std::vector<int>         las_classes_building;   // LAS classes used by buildings
+    
+    //-- Output
+    fs::path                  workDir;
+    fs::path                  outputDir         = fs::current_path();
+
+    //-- Other settings
+    double    pointCloudThinning                   = 0.;
+
+    //== Public functions
+    void read_config(std::string& config_path) {
+        std::ifstream json_file(config_path);
+        if (!json_file)
+            throw std::invalid_argument(std::string("Configuration file " + config_path + " not found."));
+
+        //-- Filepaths in the json file are relative to the location of the json file
+        this->workDir = fs::path(config_path).parent_path();
+        fs::current_path(this->workDir);
+        std::cout << "Work directory: " << fs::canonical(this->workDir) << std::endl;
+
+        nlohmann::json j;
+        try {
+            j = nlohmann::json::parse(json_file, nullptr, true, true);
+        } catch (std::exception& e) {
+            std::cerr << "ERROR: Configuration file is not a valid JSON file. "
+                      << "Check if you're missing a comma or a brace." << std::endl;
+            throw;
+        }
+
+        std::cout << "\nValidating JSON configuration file...";
+        this->validate(j);
+        std::cout <<"Configuration file is valid! \n" << std::endl;
+
+        this->set_config(j);
+    }
+
+    void validate(nlohmann::json& j) {
+        using valijson::Schema;
+        using valijson::SchemaParser;
+        using valijson::Validator;
+        using valijson::adapters::NlohmannJsonAdapter;
+        using valijson::ValidationResults;
+
+        //- Load schema
+        Schema configSchema;
+        SchemaParser parser;
+        NlohmannJsonAdapter configSchemaAdapter(jsonschema::schema);
+        // debug
+//    nlohmann::json schema = nlohmann::json::parse(std::ifstream("../../data/input/schema.json"), nullptr, true, true);
+//    NlohmannJsonAdapter configSchemaAdapter(schema);
+
+        parser.populateSchema(configSchemaAdapter, configSchema);
+
+        //- Validate with schema
+        Validator validator;
+        NlohmannJsonAdapter configAdapter(j);
+        ValidationResults results;
+        if (!validator.validate(configSchema, configAdapter, &results)) {
+            std::stringstream err_oss;
+            err_oss << "Validation failed." << std::endl;
+            ValidationResults::Error error;
+            int error_num = 1;
+            while (results.popError(error))
+            {
+                std::string context;
+                std::vector<std::string>::iterator itr = error.context.begin();
+                for (; itr != error.context.end(); itr++)
+                    context += *itr;
+
+                err_oss << "Error #" << error_num << std::endl
+                        << "  context: " << context << std::endl
+                        << "  desc:    " << error.description << std::endl;
+                ++error_num;
+            }
+            throw std::runtime_error(err_oss.str());
+        }
+    };
+
+    void set_config(nlohmann::json& j) {
+        if (j.contains("point_clouds")) {//todo schema for all of this stuff
+            if (j["point_clouds"].contains("las_datasets")) {
+                for (std::string filename : j["point_clouds"]["las_datasets"]["files"]) {
+                    las_files.push_back(filename);
+                }
+                if (j["point_clouds"]["las_datasets"].contains("ground_classes")) {
+                    for (int classID: j["point_clouds"]["las_datasets"]["ground_classes"]) {
+                        las_classes_ground.push_back(classID);
+                    }
+                }
+                if (j["point_clouds"]["las_datasets"].contains("building_classes")) {
+                    for (int classID: j["point_clouds"]["las_datasets"]["building_classes"]) {
+                        las_classes_building.push_back(classID);
+                    }
+                }
+            }
+        }
+    };
+};
+
+}
+
+#endif //CITY4CFD_PCCONFIG_H
---
+++ tools/prepare_point_cloud/src/city4cfd_prepare_point_cloud.cpp
@@ -0,0 +1,290 @@
+/*
+  City4CFD
+ 
+  Copyright (c) 2021-2022, 3D Geoinformation Research Group, TU Delft  
+
+  This file is part of City4CFD.
+
+  City4CFD is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  City4CFD is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with City4CFD.  If not, see <http://www.gnu.org/licenses/>.
+
+  For any information or further details about the use of City4CFD, contact
+  Ivan Pađen
+  <i.paden@tudelft.nl>
+  3D Geoinformation Research Group
+  Delft University of Technology
+*/
+
+#include <iostream>
+#include <chrono>
+#include <boost/locale.hpp>
+
+#include "lasreader.hpp"
+#include "CSF/src/CSF.h"
+
+#include "PCConfig.h"
+
+using namespace PCPrep;
+
+typedef std::array<double, 3> Point;
+
+void printWelcome() {
+    auto logo{
+            R"(
+     #==============================================================#
+     #                        __                                    #
+     #                   __  |''|                                   #
+     #                  |""| |''|  _   /|__                         #
+     #                __|""| |''|_| | | |""|/\_                     #
+     #               |''|""| |''|'| __| |""|'''|  _____             #
+     #          _ _  |''|""|^|''|'||""| |""|'''| |"""""|            #
+     #         |"|"| |''|""|||''|'||""| |""|'''| |"""""|            #
+     #     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~     #
+     #    ____   _   _                ___________________________   #
+     #   / ___| /_\ | |_   _   _      __  ____/___  ____/___  __ \  #
+     #  | |     |"| | __| | | | |     _  /     __  /_    __  / / /  #
+     #  | |___  |"| | |_  | |_| |  4  / /___   _  __/    _  /_/ /   #
+     #   \____| |"|  \__|  \__, |     \____/   /_/       /_____/    #
+     #                     |___/                                    #
+     #                                                              #
+     #==============================================================#
+)"
+    };
+
+    auto info{
+            R"(#===============================================================================================================#
+# Point cloud preparation tool                                                                                  #
+# This tool takes LAS/LAZ point cloud tiles and exports building and ground points                              #
+# In case the point cloud is not classified, it uses Cloth Simulation Filter (CSF) to perform ground filtering  #
+#============================================================================================================== #
+
+)"
+    };
+    std::cout << logo;
+    std::cout << "City4CFD Copyright (C) 2021-2022 3D Geoinformation Research Group, TU Delft\n" << std::endl;
+    std::cout << info;
+}
+
+void printHelp() {
+    auto helpMsg{
+            R"(USAGE:
+    city4cfd_pcprep config.json
+)"
+    };
+    std::cout << helpMsg;
+}
+
+void print_progress_bar(int percent) {
+    std::string bar;
+    for (int i = 0; i < 50; i++) {
+        if (i < (percent / 2)) {
+            bar.replace(i, 1, "=");
+        }
+        else if (i == (percent / 2)) {
+            bar.replace(i, 1, ">");
+        }
+        else {
+            bar.replace(i, 1, " ");
+        }
+    }
+    std::clog << "\r" "    [" << bar << "] ";
+    std::clog.width(3);
+    std::clog << percent << "%     " << std::flush;
+}
+
+
+int main(int argc, char** argv) {
+    try {
+        printWelcome();
+
+        auto startTime = std::chrono::steady_clock::now();
+
+        std::string config_path;
+        if (argc >= 2) {
+            config_path = fs::current_path().append(argv[1]).string();
+        } else {
+            printHelp();
+            return EXIT_SUCCESS;
+        }
+
+        //== PCPREP IMPLEMENTATION HERE
+        Config::get().read_config(config_path);
+
+        //todo when reading LAS, I have to take the scope and use middle to move all the pts!
+
+        //-- Point structures for building and ground?
+        std::vector<Point> groundPts;
+        std::vector<Point> buildingPts;
+
+        int readPts = 0;
+        for (auto& pointFile: Config::get().las_files) {
+            std::cout << "Reading LAS/LAZ file: " << pointFile << "\n";
+
+            LASreadOpener lasreadopener;
+            lasreadopener.set_file_name(pointFile.c_str());
+            //-- set to compute bounding box
+            lasreadopener.set_populate_header(true);
+            LASreader* lasreader = lasreadopener.open();
+
+            try {
+                //-- check if file is open
+                if (lasreader == nullptr)
+                    throw(std::runtime_error(std::string("Error reading LAS/LAZ file.")));
+
+                LASheader header = lasreader->header;
+
+                //-- read each point 1-by-1
+                uint32_t pointCount = header.number_of_point_records;
+
+                Point translatePt = {(header.max_x + header.min_x / 2),
+                                     (header.max_y + header.min_y / 2),
+                                     (header.max_z + header.min_z / 2)};
+
+
+                std::vector<Point> pts;
+
+                std::cout << "    " << boost::locale::as::number << pointCount << " points in the file\n";
+                double percentLeft = 1 - (Config::get().pointCloudThinning / 100);
+                if (Config::get().pointCloudThinning > 0) {
+                    std::cout << "    Skipping " << Config::get().pointCloudThinning << "% points" << std::endl;
+                }
+                else {
+                    std::cout << "    all points used, no skipping" << std::endl;
+                }
+
+                //-- Read defined classes or use CSF to determine ground from non-ground
+                if (!Config::get().las_classes_ground.empty() &&
+                    !Config::get().las_classes_building.empty()) {
+                    std::cout << "    Reading LAS classes: ";
+                    for (int i : Config::get().las_classes_ground) {
+                        std::cout << i << " ";
+                    }
+                    std::cout << "(ground) and ";
+                    for (int i : Config::get().las_classes_building) {
+                        std::cout << i << " ";
+                    }
+                    std::cout << "(buildings)" << std::endl;
+
+                    int i = 0;
+                    double currPercent = 0.;
+                    print_progress_bar(0);
+                    while (lasreader->read_point()) {
+                        LASpoint const& p = lasreader->point;
+                        //-- set the thinning filter
+                        if (currPercent >= 1.) {
+                            //-- add the point to the point cloud
+                            bool isRead = false;
+                            int classID = p.classification;
+                            std::vector<int> classList = Config::get().las_classes_ground;
+                            if (std::find(classList.begin(), classList.end(), classID)
+                                != classList.end()) {
+                                groundPts.push_back({p.get_x(), p.get_y(), p.get_z()});
+                                isRead = true;
+                            }
+                            classList = Config::get().las_classes_building;
+                            if (std::find(classList.begin(), classList.end(), classID)
+                                != classList.end()) {
+                                buildingPts.push_back({p.get_x(), p.get_y(), p.get_z()});
+                                isRead = true;
+                            }
+                            if (isRead) ++readPts;
+                            currPercent -= 1.;
+                        } else {
+                            currPercent += percentLeft;
+                        }
+                        if (i % (pointCount / 200) == 0) {
+                            print_progress_bar(100 * (i / double(pointCount)));
+                        }
+                        i++;
+                    }
+                    print_progress_bar(100);
+                    std::clog << std::endl;
+
+                    lasreader->close();
+                } else { // Cloth Simulation Filter
+                    std::cout << "    No point cloud classification information given. "
+                              << "Will filter ground using the Cloth Simulation Filter" << std::endl;
+                    CSF csf;
+                    //todo csf params setting
+                    csf.params.bSloopSmooth = true;
+                    csf.params.class_threshold = 0.5;
+                    csf.params.cloth_resolution = 2;
+                    csf.params.interations = 500;
+                    csf.params.rigidness = 3;
+                    csf.params.time_step = 0.65;
+                    //-- Read the data
+                    int i = 0;
+                    double currPercent = 0.;
+                    print_progress_bar(0);
+                    while (lasreader->read_point()) {
+                        LASpoint const& p = lasreader->point;
+                        //-- set the thinning filter
+                        if (currPercent >= 1.) {
+                            //-- Add directly to CSF's data structure
+                            csf.addPoint(p.get_x() - translatePt[0],
+                                         p.get_y() - translatePt[1],
+                                         p.get_z() - translatePt[2]);
+
+                            ++readPts;
+                            currPercent -= 1.;
+                        } else {
+                            currPercent += percentLeft;
+                        }
+                        if (i % (pointCount / 200) == 0) {
+                            print_progress_bar(100 * (i / double(pointCount)));
+                        }
+                        i++;
+                    }
+                    print_progress_bar(100);
+                    std::clog << std::endl;
+
+                    lasreader->close();
+
+                    //-- Perform CS filtering
+                    std::cout << "    Applying CSF to the point cloud" << std::endl;
+                    std::vector<int> groundIndices, offGroundIndices;
+                    csf.do_filtering(groundIndices, offGroundIndices, false);
+
+                    //-- Add points to point clouds
+                    for (auto idx : groundIndices) {
+                        groundPts.push_back({csf.getPointCloud().at(idx).x + translatePt[0],
+                                             csf.getPointCloud().at(idx).z + translatePt[1],
+                                            -csf.getPointCloud().at(idx).y + translatePt[2]});
+                    }
+                    for (auto idx : offGroundIndices) {
+                        buildingPts.push_back({csf.getPointCloud().at(idx).x + translatePt[0],
+                                               csf.getPointCloud().at(idx).z + translatePt[1],
+                                              -csf.getPointCloud().at(idx).y + translatePt[2]});
+                    }
+                }
+                std::clog << "Points read: " << readPts << "\n" << std::endl;
+            }
+            catch (std::exception& e) {
+                if (lasreader != nullptr) lasreader->close();
+                throw;
+            }
+        }
+
+        auto endTime = std::chrono::steady_clock::now();
+        auto diffTime = endTime - startTime;
+        std::cout << "\nProgram executed in " << std::chrono::duration<double>(diffTime).count() << " s" << std::endl;
+        std:: cout << "End" << std::endl;
+
+        return EXIT_SUCCESS;
+    } catch (std::exception& e) {
+
+        std::cerr << "\nProgram failed! Reason: " << e.what() << std::endl;
+        std::cout << "End" << std::endl;
+        return EXIT_FAILURE;
+    }
+}
