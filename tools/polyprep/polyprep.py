b2de84dba0ffffe20d8a06bca540036325247ce4
Ivan Pađen <49401914+ipadjen@users.noreply.github.com>
2024-05-27 18:03:25 +0200

    Add header to polyprep

--- tools/polyprep/polyprep.py
+++ tools/polyprep/polyprep.py
@@ -1,3 +1,30 @@
+"""
+  polyprep
+
+  Copyright (c) 2024, 3D Geoinformation Research Group, TU Delft
+
+  This file is part of City4CFD.
+
+  City4CFD is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  City4CFD is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with City4CFD.  If not, see <http://www.gnu.org/licenses/>.
+
+  For any information or further details about the use of City4CFD, contact
+  Ivan Pađen
+  <i.paden@tudelft.nl>
+  3D Geoinformation Research Group
+  Delft University of Technology
+"""
+
 import argparse
 from shapely.geometry import shape, mapping, MultiPolygon, Polygon
 from shapely.ops import unary_union
