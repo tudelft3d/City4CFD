f432bbb502bee0b2fca08397c587ed912849db9d
Ivan Paden <i.paden@tudelft.nl>
2025-01-03 12:10:28 +0100

    Add option to output buildings separately for obj and stl

--- src/Config.cpp
+++ src/Config.cpp
@@ -310,6 +310,11 @@ void Config::set_config(nlohmann::json& j) {
     } else throw city4cfd_error(std::string("'" + outputFormatConfig + "'" + " is unsupported file format!"));
 
     outputSeparately = j["output_separately"];
+    if (j.contains("output_buildings_separately"))
+        outputBuildingsSeparately = j["output_buildings_separately"];
+    //todo temp
+//    outputBuildingsSeparately = true;
+    if (outputBuildingsSeparately) outputSeparately = true;
 
     //-- Data log
     if (j.contains("output_log")) {
